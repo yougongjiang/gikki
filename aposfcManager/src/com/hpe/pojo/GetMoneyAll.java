b0914fd59485c29e014e8e9c09d3bddae5441d61
yougongjiang <44501668+yougongjiang@users.noreply.github.com>
2019-09-09 10:12:30 +0800

    apsfc pojo

---
+++ aposfcManager/src/com/hpe/pojo/GetMoneyAll.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package com.hpe.pojo;
+
+/** 
+ * 类描述：
+ * 作者： mujie 
+ * 创建日期：2019年9月7日
+ * 修改人：
+ * 修改日期：
+ * 修改内容：
+ * 版本号： 1.0.0   
+ */
+public class GetMoneyAll {
+	private String menuname;
+	private double sum;
+	private String price;
+	private double total;
+	private String times;
+	/**
+	 * @return the menuname
+	 */
+	public String getMenuname() {
+		return menuname;
+	}
+	/**
+	 * @param menuname the menuname to set
+	 */
+	public void setMenuname(String menuname) {
+		this.menuname = menuname;
+	}
+	/**
+	 * @return the sum
+	 */
+	public double getSum() {
+		return sum;
+	}
+	/**
+	 * @param sum the sum to set
+	 */
+	public void setSum(double sum) {
+		this.sum = sum;
+	}
+	/**
+	 * @return the price
+	 */
+	public String getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	/**
+	 * @return the total
+	 */
+	public double getTotal() {
+		return total;
+	}
+	/**
+	 * @param total the total to set
+	 */
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	/**
+	 * @return the times
+	 */
+	public String getTimes() {
+		return times;
+	}
+	/**
+	 * @param times the times to set
+	 */
+	public void setTimes(String times) {
+		this.times = times;
+	}
+	
+}
