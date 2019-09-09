e01b7ac97f45599d9efec9ed5a30400c23751f42
yougongjiang <44501668+yougongjiang@users.noreply.github.com>
2019-09-09 17:32:26 +0800

    Add files via upload

---
+++ aposfcManager/src/com/hpe/service1/INoticeService.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.hpe.service;
+
+import java.util.List;
+
+import com.hpe.pojo.Notice;
+
+/** 
+ * 类描述：
+ * 作者： qinliyu zhoukeyu wangxiaoyu
+ * 创建日期：2019年9月6日
+ * 修改人：
+ * 修改日期：
+ * 修改内容：
+ * 版本号： 1.0.0   
+ */
+public interface INoticeService {
+
+	/**
+	 * 
+	 * 方法描述：查询所有
+	 * @return 所有类别信息
+	 */
+	List<Notice> getNoticeAll();
+	
+	/**
+	 * 
+	 * 方法描述：添加公告
+	 * @param notice 公告信息
+	 * @return 影响的行数
+	 */
+	int addNotice(Notice notice);
+	
+	/**
+	 * 
+	 * 方法描述：删除公告
+	 * @param notice
+	 * @return 影响的行数
+	 */
+	int delete(int id);
+	
+	/** 
+	  * 方法描述：根据name查询
+	  * @param name
+	  * @return 公告信息
+	  */
+	 
+	 Notice getNoticeByName(String name);
+	
+}
